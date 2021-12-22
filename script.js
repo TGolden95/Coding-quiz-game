70a4262364ee331c3bc9173c97c385b75980b3c4
Triston Golden <triston_golden@yahoo.com>
2021-12-22 15:02:25 -0500

    updating code

--- script.js
+++ script.js
@@ -4,25 +4,26 @@
 //4. You must remain in the quiz while you are taking it.
 //5. You will accumulate points if your answers are correct.
 
+// Which of the following is not a JavaScript variable?
+// div
+// var
+// let
+// const
+
 // What does HTML stand for?
 // Hyper Text Preprocessor
-// Hyper Text Markup Language
-// Hyper Text Multiple Language
 // Hyper Tool Multi Language
+// Hyper Text Markup Language
+// Hey There My Lady
+
+// What does a boolean method return?
+// Number
+// True or False
+// String
+// Modulo
 
 // what does CSS stand for?
-// Common Style Sheet
-// colorful Style Sheet
+// Courageous Super Saiyan
+// Colorful Style Sheet
 // Computer Style Sheet
 // Cascading Style Sheet
-
-// what does PHP stand for?
-// Hypertext Preprocessor
-// Hypertext Programming
-// Hypertext Preprogramming
-// Hometext Preprocessor
-
-// What does SQL stand for?
-// Stylish Question Language
-// Stylish Query Language
-// Statement Question Language
