3a90fde9f4114df1ce1bc20e0214b8733624e559
Triston Golden <triston_golden@yahoo.com>
2021-12-21 23:07:54 -0500

    Added starter code

---
+++ script.js
@@ -0,0 +1,28 @@
+//1.You will have only 15 seconds to answer each question.
+//2. You can only sselct your answer once.
+//3. Once time is up you can't select anything else.
+//4. You must remain in the quiz while you are taking it.
+//5. You will accumulate points if your answers are correct.
+
+// What does HTML stand for?
+// Hyper Text Preprocessor
+// Hyper Text Markup Language
+// Hyper Text Multiple Language
+// Hyper Tool Multi Language
+
+// what does CSS stand for?
+// Common Style Sheet
+// colorful Style Sheet
+// Computer Style Sheet
+// Cascading Style Sheet
+
+// what does PHP stand for?
+// Hypertext Preprocessor
+// Hypertext Programming
+// Hypertext Preprogramming
+// Hometext Preprocessor
+
+// What does SQL stand for?
+// Stylish Question Language
+// Stylish Query Language
+// Statement Question Language
