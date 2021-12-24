7e1fc30ebf8c576ec048bf340f582aa2ec83496a
Triston Golden <triston_golden@yahoo.com>
2021-12-23 23:20:11 -0500

    Almost done

---
+++ questions.js
@@ -0,0 +1,42 @@
+let questions = [
+  {
+    numb: 1,
+    question: "Which of the following is not a JavaScript variable?",
+    answer: "div",
+    options: ["div", "var", "let", "const"],
+  },
+  {
+    numb: 2,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Tool Multi Language",
+      "Hyper Text Markup Language",
+      "Hey There My Lady",
+    ],
+  },
+  {
+    numb: 3,
+    question: "What does a Boolean method return?",
+    answer: "True or False",
+    options: ["Number", "True or False", "String", "Modulo"],
+  },
+  {
+    numb: 4,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Courageous Super Saiyan",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet",
+    ],
+  },
+  {
+    numb: 5,
+    question: "What does JS stand for?",
+    answer: "Java Script",
+    options: ["Java Script", "Jail Sentence", "Jail Script", "Java Sentence"],
+  },
+];
